35a99d883f93bd6a29af5a67b8a6f1109c27dbbe
Dean Pierrot <dapierrot21@gmail.com>
2021-02-16 17:23:14 -0500

    Refactoring code, Setting game state.

--- 02_Terminal_Hacker/02_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 02_Terminal_Hacker/02_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -4,19 +4,73 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    // Game State.
+     int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
     // Start is called before the first frame update
     void Start()
     {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local bank.");
         Terminal.WriteLine("Press 2 for the F.B.I.");
         Terminal.WriteLine("Press 3 for Area 51.");
         Terminal.WriteLine("Enter your selection: ");
     }
+    
 
-    // Update is called once per frame
-    void Update()
+    void OnUserInput(string input)
     {
-        
+        if(input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else if (input == "007")
+        {
+            Terminal.WriteLine("Choose a level Mr.Bond.");
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid level.");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have selected level " + level);
+        Terminal.WriteLine("Please enter your password: ");
     }
 }
