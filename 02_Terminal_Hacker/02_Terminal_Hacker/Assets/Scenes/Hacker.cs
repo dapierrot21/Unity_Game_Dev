94878406fca71d8840507d0e314aecd02950be63
Dean Pierrot <dapierrot21@gmail.com>
2021-02-16 19:10:22 -0500

    Adding a function to receive user's password.

--- 02_Terminal_Hacker/02_Terminal_Hacker/Assets/Scenes/Hacker.cs
+++ 02_Terminal_Hacker/02_Terminal_Hacker/Assets/Scenes/Hacker.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Hacker : MonoBehaviour
 {
     // Game State.
-     int level;
+    int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -37,6 +38,10 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            GetPassword(input);
+        }
 
     }
 
@@ -45,17 +50,21 @@ public class Hacker : MonoBehaviour
         if (input == "1")
         {
             level = 1;
+            password = "cash";
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "agent";
             StartGame();
         }
         else if (input == "3")
         {
             level = 3;
+            password = "alien";
             StartGame();
+            
         }
         else if (input == "007")
         {
@@ -72,5 +81,19 @@ public class Hacker : MonoBehaviour
         currentScreen = Screen.Password;
         Terminal.WriteLine("You have selected level " + level);
         Terminal.WriteLine("Please enter your password: ");
+    }
+
+    void GetPassword(string input)
+    {
+
+        if(input == password)
+        {
+            Terminal.WriteLine("Congrats on cracking the password!");
+        }
+        else
+        {
+            Terminal.WriteLine("Try Again...");
+            StartGame();
+        }
     }
 }
