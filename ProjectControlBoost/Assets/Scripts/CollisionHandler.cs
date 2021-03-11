cbfce01a9940f3e9dc369b039717a1dd85e2b840
Dean Pierrot <dapierrot21@gmail.com>
2021-03-11 03:02:42 -0500

    Features: Added tags to scence objects. using switch statement to load next when rocket lands on opposite landing pad. Also to reload current level whenever to the rocket crash or hits an object thats not the landing pad.

---
+++ ProjectControlBoost/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Your on the launch pad");
+                break;
+            case "Finish":
+                NextLevel();
+                break;
+            default:
+                ReloadLevel(); 
+                break;
+
+        }
+    }
+
+    private void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextScenceNumber = currentSceneIndex + 1;
+
+        if(nextScenceNumber == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScenceNumber = 0;
+        }
+
+        SceneManager.LoadScene(nextScenceNumber);
+        
+    }
+
+    // If rocket crash reload scene.
+    private static void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
